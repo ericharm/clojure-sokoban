cd693b3a8c1db551a993cd7a72cdffa13a32012e
Eric Harm <ericharm@Erics-MacBook-Pro.local>
2022-04-09 21:47:49 -0400

    replace record with regular map

--- src/clojure_sokoban/core.clj
+++ src/clojure_sokoban/core.clj
@@ -3,23 +3,24 @@
 
 (def term (t/get-terminal :swing))
 
-(defrecord Point [x y])
+(defn create-point [x y]
+  {:x x :y y})
 
 (defn handle-input []
   (t/get-key-blocking term))
 
 (defn next-location [location input]
   (case input
-    :left  (Point. (- (:x location) 1) (:y location))
-    :right (Point. (+ (:x location) 1) (:y location))
-    :up    (Point. (:x location) (- (:y location) 1))
-    :down  (Point. (:x location) (+ (:y location) 1))))
+    :left  (create-point (- (:x location) 1) (:y location))
+    :right (create-point (+ (:x location) 1) (:y location))
+    :up    (create-point (:x location) (- (:y location) 1))
+    :down  (create-point (:x location) (+ (:y location) 1))))
 
 (defn game-update [location]
   (t/move-cursor term (:x location) (:y location))
   (game-update (next-location location (handle-input))))
 
 (defn -main []
-  (let [current-location (Point. 10 10)]
+  (let [current-location (create-point 10 10)]
     (t/start term)
     (game-update current-location)))
